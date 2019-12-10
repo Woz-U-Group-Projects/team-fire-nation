eb11288e14261ae5106b73106aaec1a49958a919
Alvin <38302259+AlvinW89@users.noreply.github.com>
2019-12-10 12:16:52 -0500

    Base Registration Page created

--- src/components/screens/Registration.js
+++ src/components/screens/Registration.js
@@ -1,5 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
+import registrationImg from "../../registrationImg.svg";
 
-ReactDOM.render( <h1>Welcome, Signup below.</h1>, document.getElementById('root'))
+export class Registration extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="base-container" ref={this.props.containerRef}>
+        <div className="header">Registration</div>
+        <div className="content">
+          <div className="image">
+            <img src={registrationImg} />
+          </div>
+          <div className="form">
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input type="text" name="username" placeholder="username" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" placeholder="password" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" placeholder="Email" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="firstname">First name</label>
+              <input type="text" name="firstname" placeholder="First Name" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="lastname">Last Name</label>
+              <input type="text" name="lastname" placeholder="Last Name" />
+            </div>
+          </div>
+        </div>
+        <div className="footer">
+        <button type="button" className="btn">
+            Go back
+          </button>
+          <button type="button" className="btn">
+            Signup
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
