00fc74d9a72d9021300f118b401bd674d1c4a33d
Alvin <38302259+AlvinW89@users.noreply.github.com>
2019-12-19 19:00:05 -0500

    Save current stat

--- connected-gamers/models/users.js
+++ connected-gamers/models/users.js
@@ -1,26 +1,40 @@
-/* jshint indent: 2 */
-
-module.exports = function(sequelize, DataTypes) {
+'use strict';
+module.exports = (sequelize, DataTypes) => {
   const users = sequelize.define('users', {
     UserId: {
+      type: DataTypes.INTEGER(5).UNSIGNED,
       allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER
-    },     
-    FirstName: DataTypes.STRING,
-    LastName: DataTypes.STRING,
+      primaryKey: true
+    },
+    FirstName: {
+      type: DataTypes.STRING(45),
+      allowNull: false
+    },
+    LastName: {
+      type: DataTypes.STRING(45),
+      allowNull: false
+    },
     Email: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(45),
       unique: true
     },
     Username: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(45),
       unique: true
     },
-    Password: DataTypes.STRING,
-    Bio: DataTypes.STRING
-  }, {
-    tableName: 'users'
-  });
+    Password: {
+      type: DataTypes.STRING(45),
+      unique: true
+    },
+    Bio: {
+      type: DataTypes.STRING(150),
+      unique: true
+    }
+  }, {});
+  users.associate = function(models) {
+    // associations can be defined here
+  };
+  return users;
 };
+
+
