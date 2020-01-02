1c5161d856c9fdabe1778e2377b1539bad1713d9
benclay1990 <benjamin_joe_clay_temple@hotmail.com>
2020-01-02 15:30:57 -0700

    add about info

--- client-react/src/screens/About.js
+++ client-react/src/screens/About.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import Header from '../component/Header';
 
-const About = () => <Header title="About" />;
+const About = () => 
+(
+<div>
+
+    <Header title="About" />
+    <p>Welcome Gamers!  Connected Gamers is a website where you, the gamers, can communicate and catch up on the latest trends and gaming topics on the display feed. With this social platform gamers can display information about themselves, add other profiles to friends list, share information, and stay connected with other gamers.</p>
+
+</div>
+);
 
 export default About;
