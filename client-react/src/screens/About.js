7ca0e7d1b8e4360af0e0896114b5617e7815a994
Alvin <38302259+AlvinW89@users.noreply.github.com>
2020-01-06 11:22:41 -0500

    Boilerplate React and express app

--- client-react/src/screens/About.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Header from '../component/Header';
-
-const About = () => <Header title="About" />;
-
-export default About;
--- client-react/src/screens/Comments.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Header from '../component/Header';
-
-const Comments = () => <Header title="Comments" />;
-
-export default Comments;
