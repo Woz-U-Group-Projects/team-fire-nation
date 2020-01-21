b54db008588342a64190ec3fd30189b8d992d562
benclay1990 <benjamin_joe_clay_temple@hotmail.com>
2020-01-20 18:43:17 -0700

    minor fixes

--- client/src/screens/Forum.js
+++ client/src/screens/Forum.js
@@ -6,9 +6,7 @@ const Forum = () => (
         <Header title="Forum" />
         <div class="card">
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">Cras justo odio</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Vestibulum at eros</li>
+                <li class="list-group-item">Coming Soon!</li>
             </ul>
         </div>
     </div>
--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -11,27 +11,17 @@ const Home = () => (
         <div className="row">
             <div className="col col-md-6">
                 <h3>Feed: Did you know gaming?</h3>
-                <a className="twitter-timeline" href="https://twitter.com/didyouknowgamin?ref_src=twsrc%5Etfw">Tweets by didyouknowgamin</a> 
+                <a className="twitter-timeline" height="700" href="https://twitter.com/didyouknowgamin?ref_src=twsrc%5Etfw">Tweets by didyouknowgamin</a> 
             </div>
             <div className="col col-md-6">
                 <div className="row">
                     <div className="col-sm-12">
-                        <h4>Lorem ipsum dolor sit</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit amet ad laboriosam provident accusantium expedita hic atque, nostrum rem quas blanditiis nobis numquam, quia deleniti nesciunt ratione ea natus possimus?</p>
+                        <h4>Latest Forum Posts</h4>
+                        <p>Forum coming soon!!</p>
                         <hr />
                     </div>  
                     
-                    <div className="col-sm-12">
-                        <h4>Lorem ipsum dolor sit</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit amet ad laboriosam provident accusantium expedita hic atque, nostrum rem quas blanditiis nobis numquam, quia deleniti nesciunt ratione ea natus possimus?</p>
-                        <hr />
-                    </div> 
-
-                    <div className="col-sm-12">
-                        <h4>Lorem ipsum dolor sit</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit amet ad laboriosam provident accusantium expedita hic atque, nostrum rem quas blanditiis nobis numquam, quia deleniti nesciunt ratione ea natus possimus?</p>
-                        <hr />
-                    </div> 
+                   
                 </div>
             </div>
         </div>
